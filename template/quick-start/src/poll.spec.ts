6b014591dc54a294e2d0da5844de71be7e86322d
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-05-08 16:23:08 +0200

    upd

--- template/quick-start/src/poll.spec.ts
+++ template/quick-start/src/poll.spec.ts
@@ -1,22 +1,48 @@
 import "@ton/test-utils";
 import { toNano } from "@ton/core";
-import { Blockchain } from "@ton/sandbox";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { Poll } from "../output/poll_Poll";
 
-it("should deploy correctly", async () => {
-    const blockchain = await Blockchain.create();
+describe("Poll contract", () => {
+    let blockchain: Blockchain;
+    let treasury: SandboxContract<TreasuryContract>;
 
-    const contract = blockchain.openContract(await Poll.fromInit());
-
-    const deployer = await blockchain.treasury("deployer");
-
-    // Send a message that `receive()` would handle
-    const result = await contract.send(deployer.getSender(), { value: toNano(1) }, null);
-
-    expect(result.transactions).toHaveTransaction({
-        from: deployer.address,
-        to: contract.address,
-        deploy: true,
-        success: true,
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        blockchain.verbosity.print = false;
+        treasury = await blockchain.treasury("deployer");
     });
+
+    it("should be deployed correctly", async () => {
+        // Prepare a contract wrapper with initial data given as 0n, 0n, 0n:
+        // ID, option0, and option1 all set to 0
+        const contract = blockchain.openContract(await Poll.fromInit(0n, 0n, 0n));
+
+        // Send a message that `receive()` would handle
+        const sendResult = await contract.send(treasury.getSender(), { value: toNano(1) }, null);
+
+        // Expect a successful deployment
+        expect(sendResult.transactions).toHaveTransaction({
+            from: treasury.address,
+            to: contract.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it("should deploy Voter contracts correctly", async () => {
+        const contract = blockchain.openContract(await Poll.fromInit(0n, 0n, 0n));
+        let sendResult = await contract.send(treasury.getSender(), { value: toNano(1) }, null);
+        expect(sendResult.transactions).toHaveTransaction({
+            from: treasury.address,
+            to: contract.address,
+            deploy: true,
+            success: true,
+        });
+
+        // ...
+    });
+
+    // NOTE: To add your own tests, simply copy-paste any of the `it()` clauses
+    //       and adjust the logic to match your expected outcomes!
 });
