cb41a7d8a0dc22656b0faa4f4d1d489feff06080
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-05-09 12:13:02 +0200

    test

--- template/quick-start/src/poll.spec.ts
+++ template/quick-start/src/poll.spec.ts
@@ -40,7 +40,45 @@ describe("Poll contract", () => {
             success: true,
         });
 
-        // ...
+        // Let's create Toncoin wallets for two voters
+        const wallet0 = await blockchain.treasury("voter-wallet-0");
+        const wallet1 = await blockchain.treasury("voter-wallet-1");
+
+        // And a small helper function for our needs
+        const voteFrom = async (
+            wallet: SandboxContract<TreasuryContract>,
+            option: 0n | 1n,
+            success = true,
+        ) => {
+            const res = await contract.send(wallet.getSender(), { value: toNano(1) }, {$$type: 'Vote', option });
+            expect(res.transactions).toHaveTransaction({
+                from: wallet.address,
+                to: contract.address,
+                success,
+            });
+        };
+
+        // Vote for option 0 from the first voter's wallet
+        await voteFrom(wallet0, 0n);
+
+        // Vote for option 1 from the second voter's wallet
+        await voteFrom(wallet1, 1n);
+
+        // There should be a vote for option one and option two
+        const state = await contract.getState();
+        expect(state.option0).toBe(1n);
+        expect(state.option1).toBe(1n);
+
+        // Finally, voters should not be able to vote more than once.
+        // That is, the resulting state after new vote attempts should not change.
+        await voteFrom(wallet0, 0n); // repeat previous vote
+        await voteFrom(wallet0, 1n); // attempt a different vote
+        await voteFrom(wallet1, 1n); // repeat previous vote
+        await voteFrom(wallet1, 0n); // attempt a different vote
+
+        const finalState = await contract.getState();
+        expect(finalState.option0).toBe(state.option0);
+        expect(finalState.option1).toBe(state.option1);
     });
 
     // NOTE: To add your own tests, simply copy-paste any of the `it()` clauses
