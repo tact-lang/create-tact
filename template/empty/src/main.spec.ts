0a3c6fce0e6938b85249252f5a9c7985a5dc3311
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-05-13 11:13:00 +0200

    chore: revert changes to empty template in order to apply them in another PR

--- template/empty/src/main.spec.ts
+++ template/empty/src/main.spec.ts
@@ -5,10 +5,12 @@ import { ContractName } from "../output/projectName_ContractName";
 
 it("should deploy correctly", async () => {
     const blockchain = await Blockchain.create();
+
     const contract = blockchain.openContract(await ContractName.fromInit());
+
     const deployer = await blockchain.treasury("deployer");
 
-    // Send a message that `receive()` would handle
+    // call `receive()`
     const result = await contract.send(deployer.getSender(), { value: toNano(1) }, null);
 
     expect(result.transactions).toHaveTransaction({
